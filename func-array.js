e082ce8f60ff4db6d771cbdea1f1156a8064f2cb
deepanshubisht2002-byte <deepanshubisht2002@gmail.com>
2025-11-29 21:11:40 +0530

    Removed Past unrelatetd comments and added a new comment

--- func-array.js
+++ func-array.js
@@ -1,11 +1,4 @@
-// // function that takes an array as input and display all items of this array
-// function listArrayItems(arr) {
-//     for (var i = 0; i < arr.length; i++) {
-//         console.log(i, arr[i])
-//     }
-// }
-// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
-// listArrayItems(colors);
+//A simple program to find a letter in a string
 letterFinder("test","t");
 
 function letterFinder(word,match) {
