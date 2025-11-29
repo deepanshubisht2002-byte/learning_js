b37ab417a05f56f5e5e131e5568d167690538f6d
deepanshubisht2002-byte <deepanshubisht2002@gmail.com>
2025-11-29 21:23:29 +0530

    Created my First Object

---
+++ objetcss.js
@@ -0,0 +1,17 @@
+//Just learned about objects
+var player = {};
+player.health=100;
+player.Attack=50;
+player.Defense=50;
+player.Speed=80;
+player.Money=5000;
+console.log("Player stats:",player);
+
+var rival = {
+    health: 100,
+    Attack: 70,
+    Defense: 60,
+    Speed: 100,
+    Money: 5000
+}
+console.log("Rival stats:",rival);
